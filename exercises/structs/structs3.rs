f11362fd3c584c9ba4781d5841d7b05dc33d082d
zhangjiangwen <libai7236@gmail.com>
2023-04-04 18:34:24 +0800

    🧪 test: ✅ move structs enums

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -4,8 +4,6 @@
 // Make the code compile and the tests pass!
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Package {
     sender_country: String,
@@ -14,7 +12,7 @@ struct Package {
 }
 
 impl Package {
-    fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
+    fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Self {
         if weight_in_grams <= 0 {
             panic!("Can not ship a weightless package.")
         } else {
@@ -26,12 +24,16 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        if self.sender_country != self.recipient_country {
+            true
+        } else {
+            false
+        }
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        cents_per_gram * self.weight_in_grams
     }
 }
 
