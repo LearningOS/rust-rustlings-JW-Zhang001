5919a947e6281b6e5b27842e5e5b792aba1a49fb
zhangjiangwen <libai7236@gmail.com>
2023-04-10 11:46:35 +0800

    🧪 test: errors、traits

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -11,13 +11,17 @@
 // you can do this!
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-//TODO: Add your code here
+// Add your code here
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self {
+        self.push("Bar".to_string());
+        self
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -28,5 +32,27 @@ mod tests {
         let mut foo = vec![String::from("Foo")].append_bar();
         assert_eq!(foo.pop().unwrap(), String::from("Bar"));
         assert_eq!(foo.pop().unwrap(), String::from("Foo"));
+    }
+
+    #[test]
+    fn is_vec_len_eq_1() {
+        let foo = vec![String::from("Foo")].append_bar();
+        assert_eq!(foo.len(), 2);
+    }
+    #[test]
+    fn is_vec_len_eq_2() {
+        let foo = vec![String::from("Foo"), String::from("Bar")].append_bar();
+        assert_eq!(foo.len(), 3);
+    }
+
+    #[test]
+    fn is_vec_len_eq_3() {
+        let foo = vec![
+            String::from("Foo"),
+            String::from("Bar"),
+            String::from("Bar"),
+        ]
+        .append_bar();
+        assert_eq!(foo.len(), 4);
     }
 }
